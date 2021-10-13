13be7df34a2348e6e332b9b50e8c704dfa74e8d6
georgeleve <leventopoulos.george@gmail.com>
2021-10-13 03:41:17 +0300

    dafq

--- ex1/exercise2/main.js
+++
@@ -1,6 +0,0 @@
-var firstPassword = document.getElementById("pwd").value;
-var secondPassword = document.getElementById("pwd2").value;
-
-if (firstPassword !== secondPassword) {
-    alert("Error, passwords are not the same, please try again.\n")
-}
