ca00664d716f7cba66cd60f77beee5bfc652b7c9
georgeleve <leventopoulos.george@gmail.com>
2021-10-20 16:57:52 +0300

    change html

--- A1_4152/assignment1/main.js
+++ A1_4152/assignment1/main.js
@@ -1,1 +1,37 @@
 console.log("Hello world !!!");
+
+var str = "my name is george";
+typeof(str);
+
+console.log(Number('1')+Number('2'));
+
+var str = '8';
+var num = 8;
+console.log(str == num)
+// type coersion
+// returns true
+
+function myFunction(txt){
+    alert(txt);
+}
+
+var i;
+for (i=0; i < 10; i++){
+    console.log(i);
+}
+
+document.write("hellow world in js");
+
+function changeMessage(){
+    document.getElementById("mytext").innerHTML = "Message changed successfully!!!";
+}
+
+document.write(5 + 6);
+// alert(typeof(i));
+
+let x, y, z;
+x = 4;
+y = 9;
+z = x + y;
+
+document.write("z=", z);
--- A1_4152/assignment2/main.js
+++ A1_4152/assignment2/main.js
@@ -1,6 +1,9 @@
-// var firstPassword = document.getElementById("pwd").value;
-// var secondPassword = document.getElementById("pwd2").value;
+function checkPasswords(){
+    var firstPassword = document.getElementById("password1").value;
+    var secondPassword = document.getElementById("password2").value;
 
-// if (firstPassword !== secondPassword) {
-//     alert("Error, passwords are not the same, please try again.\n")
-// }
+    if (firstPassword !== secondPassword) {
+        alert("Error! \"Password\" and \"Password Confirmation\" must be the same, please try again.\n")
+    }
+}
+
