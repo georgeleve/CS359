b26b3ba9212c33af1fc30fa841ff99854cbd503b
leventopoulos.george <leventopoulos.george@gmail.com>
2021-11-14 07:51:21 +0200

    almsot finish ask3

--- A2_4152/assignment3/main.js
+++ A2_4152/assignment3/main.js
@@ -1,135 +1,194 @@
-const game = {
-    plays: "X",
-    moves: 0,
-    cells: [],
-    winner: false
-};
+/* Georgios Gerasimos Leventopoulos csd4152  */
+var grid = [];
+var currentColor = 'red';
+var activeGame = false;
+var filledCircles = 0;
+var redPlayerWins = 0;
+var yellowPlayerWins = 0;
+var totalDraws = 0;
 
-game.cells[0]=[null,null,null,null,null,null,null,null];
-game.cells[1]=[null,null,null,null,null,null,null,null];
-game.cells[2]=[null,null,null,null,null,null,null,null];
-game.cells[3]=[null,null,null,null,null,null,null,null];
-game.cells[4]=[null,null,null,null,null,null,null,null];
-game.cells[5]=[null,null,null,null,null,null,null,null];
+// Bug: na ftiaxo thn kato kato grammh sto  vertically
+// na anavosbhnoun ama kerdisei kapoios
+// na ftiaxo to infobox
+// na xanado to updatePage
+// na kano to bonus
 
 function newGame(){
-    document.getElementById("message").innerHTML += 
-    "Infobox Player: Total Moves: Empty Cells: Draws: Red Player Wins: Yellow Player Wins: <br>";
-    //initialize infobox, variables etc
+    grid = [
+        [{},{},{},{},{},{},{}],
+        [{},{},{},{},{},{},{}],
+        [{},{},{},{},{},{},{}],
+        [{},{},{},{},{},{},{}],
+        [{},{},{},{},{},{},{}],
+        [{},{},{},{},{},{},{}]
+    ];
+    activeGame = true;
+    filledCircles = 0;
+    currentColor = 'red';
+    document.getElementById("showWinner").innerHTML = "-";
+    updatePage();
 }
 
-//Event handler when someone presses a button
-function play(x,y){
-    document.getElementById('p'+x+'_'+y).innerText = game.plays;
-    document.getElementById('p'+x+'_'+y).disabled = true;
-    document.getElementById('p'+x+'_'+y).innerHTML = "<img src='redPawn.png'/>";
-    game.cells[x][y] = game.plays;
-    game.moves++;
-    //this.checkWinner(x,y);
-    if(game.winner===false){
-        changePlayerTurn();
-    }
+function updateInfobox() {
+    document.getElementById("infobox").innerHTML = 
+    " Red player wins: " + redPlayerWins + "<br>" +
+    " Yellow player wins: " + yellowPlayerWins + "<br>" +
+    " Draws: " + totalDraws;
 }
 
-function changePlayerTurn(){
-    if(game.plays=='X')
-        game.plays='O';
-    else
-        game.plays='X';
-}
-
-//checks if this move is valid
-function isValidMove(){
-    return true;
-    return false;
-}
-
-function hasPlayerWon(x,y){
-    if(game.moves>=5 && (horizontalWin(x)==true || verticalWin(y)==true || diagonialWin()==true)){
-        game.winner=true;
-        disableButtons();
-    }
-    else if(game.moves===9){
-        document.getElementById('message').innerHTML+="Draw";
-    }
-}
-
-function verticalWin(col){
-    const temp=[game.cells[0][col],game.cells[1][col],game.cells[2][col],game.cells[3][col]];
-    if(temp.filter(filterFunc).length===3){
-        return true;
-    }
-    return false;
-}
-
-function horizontalWin(row){
-    if(game.cells[row].filter(filterFunc).length===4){
-        return true;
-    }
-    return false;
-}
-
-function diagonialWin(){
-    if(game.cells[0][0]==game.cells[1][1] && game.cells[1][1]==game.cells[2][2] && game.cells[2][2]==game.plays){
-        return true;
-    }
-    else if(game.cells[0][2]==game.cells[1][1] && game.cells[1][1]==game.cells[2][0] && game.cells[2][0]==game.plays){
-        return true;
-    }
-    return false;
+function updateGoogleChartInfo(){
+    google.charts.load("current", {packages:["corechart"]});
+    google.charts.setOnLoadCallback(drawChart);
 }
 
 function isDraw(){
-    //checks if all of the deck is full
-
-
-}
-
-//may also use jquery
-function updatePage(){
-    //updates the deck
-    //if the move is valid write to the infobox:
-        //who played
-        //how much time it took using date  var start = new Date().getTime();
-        
-        var start = new Date().getTime();
-        for (i = 0; i < 50000; ++i) {
-        // do something
-        }
-        var end = new Date().getTime();
-        alert('Execution time: ' + (end-start));
-////////////////////////////
-}
-
-function showWinningMessageAndBlink(){
-
+    if(filledCircles === 42){
+        totalDraws++;
+        updateInfobox();
+        updateGoogleChartInfo();
+        return true;
+    }
+    return false;
 }
 
 function disableButtons(){
-    for(var i=0;i<3;i++){
-        for(var j=0;j<3;j++){
-            document.getElementById('p'+i+'_'+j).disabled=true;
+    activeGame = false;
+}
+
+function showWinner(color){
+    document.getElementById("showWinner").innerHTML = color + " player is the winner! Press New Game to play again";
+    updateInfobox();
+    updateGoogleChartInfo();
+    disableButtons();
+}
+
+function updatePage() {
+    let circles = "";
+    const svg = document.getElementById("svg");
+    for(var i = 0; i < grid.length; i++){
+        var row = grid[i];
+        for (var j = 0; j < row.length; j++){
+            const circle = grid[i][j];
+            const color = circle && circle.color || 'blanchedalmond';
+            circles = circles + `<circle onclick="clickCircle(${j})" fill="${color}" r="29px" cx="${j * 70 + 60}px" cy="${i * 70 + 30}px"> </circle>`;
+        }
+    }
+    svg.innerHTML = circles;
+}
+
+function changePlayerTurn(currentColor){
+    if (currentColor === 'red')
+        return 'yellow';
+    return 'red';
+}
+
+function verticalWin(i, j, grid, circle){
+    if (grid[i+1][j].color === circle.color && grid[i+2][j].color === circle.color && grid[i+3][j].color === circle.color){
+        return true;
+    }
+    return false;
+}
+
+function horizontalWin(i, j, grid, circle){
+    if(grid[i][j+1].color === circle.color && grid[i][j+2].color === circle.color && grid[i][j+3].color === circle.color){
+        return true;
+    }
+    return false;
+}
+
+function diagonialWin(i, j, grid, circle){
+    if(j === 0 || j === 1 || j === 2 || j === 3) {
+        if(grid[i+1][j+1].color === circle.color && grid[i+2][j+2].color === circle.color && grid[i+3][j+3].color === circle.color){
+            return true;
+        }
+    }
+    if(j === 3 || j === 4 || j === 5 || j === 6) {
+        if(grid[i+1][j-1].color === circle.color && grid[i+2][j-2].color === circle.color && grid[i+3][j-3].color === circle.color){
+            return true;
+        }
+    }
+    return false;
+}
+function updateStats(color){
+    if (color === 'red'){
+        redPlayerWins++;
+    }else{
+        yellowPlayerWins++;
+    }
+}
+function hasPlayerWon(){
+    if(isDraw()){
+        document.getElementById("showWinner").innerHTML = "Draw !!! Press New Game to play again";
+        disableButtons();
+        return;
+    }
+    for(var i=0; i < grid.length; i++){
+        var row = grid[i];
+        for (var j = 0; j < row.length; j++){
+            var circle = grid[i][j];
+            if(circle && circle.color){
+                if(j === 0 || j === 1 || j === 2 || j === 3) {
+                    if(horizontalWin(i, j, grid, circle)){
+                        updateStats(circle.color);
+                        showWinner(circle.color);
+                        return;  
+                    }
+                }
+                if(i===0 || i === 1){
+                   if(verticalWin(i, j, grid, circle)){
+                        updateStats(circle.color);
+                        showWinner(circle.color);
+                        return;
+                   }
+                   if(diagonialWin(i, j, grid, circle)){
+                        updateStats(circle.color);
+                        showWinner(circle.color);
+                        return;
+                   }
+                }
+            }
+        }
+    }
+}
+function getPlayerTurn(){
+    return currentColor;
+}
+
+function isValidMove(i,j){
+    if(!grid[i][j].color){
+        return true;
+    }
+    return false;
+}
+
+function clickCircle(j) {
+    if(activeGame === false) return;
+    
+    for(var i = grid.length-1; i >= 0; i--){
+        if(isValidMove(i,j)) {
+            grid[i][j] = {color:currentColor};
+            currentColor = changePlayerTurn(currentColor);
+            updatePage();
+            filledCircles++;
+            hasPlayerWon();
+            return;
         }
     }
 }
 
-function filterFunc(value){
-    return value===game.plays;
-}
+function drawChart() {
+  var data = google.visualization.arrayToDataTable([
+    ['Event', 'Times'],
+    ['Draws', totalDraws],
+    ['Red Player Wins', redPlayerWins],
+    ['Yellow Player Wins', yellowPlayerWins],
+  ]);
 
+  var options = {
+    title: 'Game Statistics',
+    is3D: true,
+  };
 
-// g) bonus
-
-function newGameAnytime(){
-
-
-}
-
-function calculatePlayerWins(player){
-
-}
-
-function functionShowGoogleCharts(){
-    yellow_player_wins = 0;
-    yellow_player_wins = 0;
+  var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
+  chart.draw(data, options);
 }
